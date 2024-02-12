ae58cac41b0d356f0ac0fc32ad418da5ddda93ab
learndaws <learndaws@gmail.com>
2024-02-12 17:29:58 +0530

    session-1:ec2

--- session-1/ec2/ec2-creation.tf
+++ session-1/ec2/ec2-creation.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "TF-AWS-Instance-Creation" {
     ami = "ami-0f3c7d07486cad139"
     instance_type = "t2.micro"
-    vpc_id = "vpc-0856fa180ad5941c7"
+    subnet_id = "subnet-07aa2a6b37659a333"
 
     tags = {
 
