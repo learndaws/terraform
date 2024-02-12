dfa27752471d26d4735e242f9a0b472e392bb135
learndaws <learndaws@gmail.com>
2024-02-12 18:33:24 +0530

    session-1:ec2 instance creation with variables

--- session-1/ec2/ec2-creation.tf
+++ session-1/ec2/ec2-creation.tf
@@ -1,9 +1,35 @@
-resource "aws_instance" "TF-AWS-Instance-Creation" {
-    ami = "ami-0f3c7d07486cad139"
-    instance_type = "t2.micro"
+resource "aws_instance" "web" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.Security-Group.id]
+
+  tags = {
+    Name = "ec2-12-Feb-2024"
+  }
+}
+
+resource "aws_security_group" "Security-Group" {
+  name        = var.SG_AWS_name
+  description = var.SG_AWS_name
+  # vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = var.SG_ingress_AWS_name
+    from_port        = var.Allow_all
+    to_port          = var.Allow_all
+    protocol         = "-1"
+    cidr_blocks      = var.CIDR_Blocks
+  }
+
+  egress {
+    description     = var.SG_egress_AWS_name 
+    from_port        = var.Allow_all
+    to_port          = var.Allow_all
+    protocol         = "-1"
+    cidr_blocks      = var.CIDR_Blocks
+  }
 
     tags = {
-
-        Name = "AWS-EC2-Instance"
-    }
+    Name = "SG-allow-all"
+  }
 }
