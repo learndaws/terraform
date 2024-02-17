b190f1485254af73ffd7d7ea78f560f517700e22
learndaws <learndaws@gmail.com>
2024-02-17 23:42:15 +0530

    data-source

--- data-source/main.tf
+++ data-source/main.tf
@@ -2,8 +2,9 @@ data "aws_ami" "centos8" {
   most_recent = true
   owners = ["973714476881"]
 
-  tags = {
-    Name   = "Centos-8-DevOps-Practice"
+  filter {
+    name = "name"
+    values = ["Centos-8-DevOps-Practice"]
   }
 }
 
